708c107ea86972baa09db7fde50f52b07b6a3950
Harshith1304 <gajula.harshith13@gmail.com>
2025-06-09 21:59:31 +0530

    Fix: updation of metadata page

--- pages/metadata.js
+++ pages/metadata.js
@@ -3,10 +3,14 @@ import Head from 'next/head';
 import dynamic from 'next/dynamic';
 import Header from '../components/Header';
 
-// Dynamically import the main view with SSR turned off
+// This dynamically imports the main view with Server-Side Rendering (SSR) turned OFF.
+// The loading property provides a fallback while the component is being loaded in the browser.
 const MetaDataView = dynamic(
   () => import('../components/MetaDataView'),
-  { ssr: false }
+  { 
+    ssr: false,
+    loading: () => <p className="text-center text-gray-400">Loading Inspector...</p>
+  }
 );
 
 const MetadataPage = () => {
@@ -16,7 +20,9 @@ const MetadataPage = () => {
                 <title>SOLdrop - Token Metadata</title>
                 <meta name="description" content="Inspect SPL Token Metadata on Solana." />
             </Head>
+            {/* We pass dummy functions because this page doesn't need to open popups */}
             <Header openTokenCreator={() => {}} openAirdrop={() => {}} />
+            
             <MetadataView />
         </div>
     );
